4bef721cc9302cb0a54217a62e62a7e5d0bd8efd
keemHwa <keem2@naver.com>
2023-07-26 00:16:27 +0900

    :memo: Node - 디렉토리의 중요성

---
+++ web-bootcamp/NODE/shelter-app.js
@@ -0,0 +1,2 @@
+const cats = require('./shelter') // 해당 디렉토리의 메인 파일 인 ./shelter/index.js 가 내보내는 항목을 가지고 오게된다. 
+console.log (cats);
---
+++ web-bootcamp/NODE/shelter/index.js
@@ -0,0 +1,9 @@
+const blue = require('./blue');
+const sadie = require('./sadie');
+const janet = require('./janet');
+
+const allcats = [blue, sadie, janet];
+
+console.log("index.js 출력 ...");
+
+module.exports = allcats;
