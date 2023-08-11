39456acf5bc7d7a6399267b076ab0a3dd6afc2b8
keemHwa <keem2@naver.com>
2023-08-11 15:29:40 +0900

    :memo: Mongoose 인스턴스 메서드

--- web-bootcamp/MongooseBasics/product.js
+++ web-bootcamp/MongooseBasics/product.js
@@ -12,12 +12,12 @@ const productSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true, // 디폴트 설정 
-        maxlength:20 
+        maxlength: 20
     },
     price: {
         type: Number, // 유효성 체크처럼 보일 수 있지만 숫자를 전달하도록 요구하지는 않는다, 숫자로 바꿀 수 있는 문자열을 요구 .. price:'599' 도 가능 
         required: true,
-        min : [0, 'Price must be positive!'] // 커스텀 메세지  price: ValidatorError: Price must be positive!
+        min: [0, 'Price must be positive!'] // 커스텀 메세지  price: ValidatorError: Price must be positive!
     },
     onSale: {
         type: Boolean,
@@ -27,20 +27,51 @@ const productSchema = new mongoose.Schema({
     qty: {
         online: {
             type: Number,
-            default : 0
+            default: 0
         },
-        inStore:  {
+        inStore: {
             type: Number,
-            default : 0
+            default: 0
         }
     }, // 두개의 하위 특성이 있는 객체
     size: {
         type: String,
-        enum : ['S','M','L'] // enum 문자열 값으로 된 배열을 제공한 후에 해당 값이 그 배열 안에 있는지 확인하는 유효성 검사
-    } 
-})
+        enum: ['S', 'M', 'L'] // enum 문자열 값으로 된 배열을 제공한 후에 해당 값이 그 배열 안에 있는지 확인하는 유효성 검사
+    }
+});
+
+
+productSchema.methods.greet = function () { // 인스턴스 메서드 ( 중복되는 부분을 줄여서 코드를 재사용 가능하게 하고 모듈화 함)
+    //console.log("HELLO ! ! !!  안녕 "); 
+    console.log(`Greet ! ${this.name}`);  //  화살표가 아닌 기존의 함수 표현식을 쓰도록 해야 한다. ( 보통 호출 객체를 가르키기에 화살표 함수 내에서 this는 호출한 객체가 아니라 호출한 객체의 상위 -> window객체를 참조한다. )
+}
+
+productSchema.methods.toggleOnSale = function () {
+    this.onSale = !this.onSale;
+    return this.save(); // save 후 thenable 반환 thenable은 promise 동작해서 다른곳에서 기다릴 수있다. 
+    
+}
+
+productSchema.methods.addCategory = function (newDog) {
+    this.categoried.push(newDog);
+    return this.save();
+    
+}
+
 
 const Product = mongoose.model('Product', productSchema); 
+
+const findProudct = async () => {
+    const foundProudct = await Product.findOne({ name: 'Mountain Bike' }); // 반환하는게 productSchema  
+    console.log(foundProudct);
+    //foundProudct.greet();
+    await foundProudct.toggleOnSale();
+    console.log(foundProudct);
+    await foundProudct.addCategory('Outdoors');
+    console.log(foundProudct);
+}
+
+findProudct();
 
 // const bike = new Product({ name: 'Mountain Bike', price: 599 })
 // const bike = new Product({ name: 'Mountain Bike', price:'HELL' })// Error: Product validation failed: price: Cast to Number failed for value "HELL" (type string) at path "price"
@@ -49,18 +80,17 @@ const Product = mongoose.model('Product', productSchema);
 
 // const bike = new Product({ name: 'Bike Helemt From Helmet Maker', price: 29.50 }) // Error: Product validation failed: name: Path `name` (`Bike Helemt From Helmet Maker`) is longer than the maximum allowed length (20).
 // const bike = new Product({ name: 'Bike Helemt', price: -9.50 }) //  price: ValidatorError: Path `price` (-9.5) is less than minimum allowed value (0).
- const bike = new Product({ name: 'Cycling Jersey', price: 28.50 , categoried:['Cycling', 'Safety'], size:'XS'}) // enum 테스트   size: ValidatorError: `XS` is not a valid enum value for path `size`.
+// const bike = new Product({ name: 'Cycling Jersey', price: 28.50 , categoried:['Cycling', 'Safety'], size:'XS'}) // enum 테스트   size: ValidatorError: `XS` is not a valid enum value for path `size`.
 
-
-bike.save()
-    .then(data => {
-        console.log("IT WORKED");
-        console.log(data);
-    })
-    .catch(err => {
-        console.log("OH NYO ! ERROR ! ");
-        console.log(err);
-    })
+// bike.save()
+//     .then(data => {
+//         console.log("IT WORKED");
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log("OH NYO ! ERROR ! ");
+//         console.log(err);
+//     })
 
 
 // const bike = new Product({ name: 'Tire Pump', price: 19.50, categoried: ['Cycling'] })
