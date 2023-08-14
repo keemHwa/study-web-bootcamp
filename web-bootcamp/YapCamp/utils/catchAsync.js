4779e4c41d40c01a4dd0c1dd210fa4a0cb0ac2a1
keemHwa <keem2@naver.com>
2023-08-14 23:19:17 +0900

    :goal_net: YapCamp : 오류 및 데이터 검증하기

---
+++ web-bootcamp/YapCamp/utils/catchAsync.js
@@ -0,0 +1,13 @@
+// function catchAsync(fc) {
+//     return function (req, res, nex) {
+//         fn(req, res, next).catch(e => next(e) );
+//     }
+// }
+
+module.exports = fn => {
+    return (req, res, next) => {
+        fn(req, res, next).catch(e => next(e) );
+    }
+}
+
+//  화살표 함수는 원래 익명함수로만 사용할 수있다. 
