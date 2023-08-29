da434ab194e73bf9bd2ef0b4b4e9ab384e92c1ef
keemHwa <keem2@naver.com>
2023-08-29 20:24:24 +0900

    :memo: UseMemo - 연산 최적화

--- one-bite-react/simple-diary/src/App.js
+++ one-bite-react/simple-diary/src/App.js
@@ -1,12 +1,9 @@
-import { useState,useRef, useEffect } from 'react';
+import { useMemo, useState,useRef, useEffect } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 // import Lifecycle from './Lifecycle';
 // import LifecycleUnmount from './LifecycleUnmount';
-
-
-// https://jsonplaceholder.typicode.com/posts/1/comments
 
 function App() {
 
@@ -61,11 +58,34 @@ function App() {
     
   }
 
+  // 감정 점수 비율
+  const getDiaryAnalysis = useMemo(
+    () => {
+      // useMemo React의 Hook 중 하나로
+        // 계산 비용이 높은 연산의 결과를 캐싱하고, 이전 결과와 다를 때만 다시 계산하는 데 사용
+        // 두번째 인자(data.length)로 넘긴 데이터에 변동이 있을 경우에만 콜백 함수 실행
+        // 또한 콜백 함수가 리턴하는 값을 return 한다. 
+      console.log("일기 분석 시작");
+      // 해당 로그 2번 호출 
+      // -> 1. App 컴포넌트가 처음 mount 될 때 출력
+      // -> 2. getDate() api가 성공 후 setData가 이루어 지면서  App 컴포넌트가 리렌더되면서 그안에 있는 함수가 재생성 될 때                                
+      const goodCount = data.filter((it) => it.emotion >= 3).length;
+      const badCount = data.length - goodCount;
+      const goodRatio = (goodCount / data.length) * 100;
+      return { goodCount, badCount, goodRatio };
+    },[data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
       {/* <Lifecycle/> */}
       {/* <LifecycleUnmount/> */}
       <DiaryEditor onCreate={onCreate} />
+      <div> 전체 일기 : {data.length} </div>
+      <div> 기분 좋은 일기 개수 : {goodCount} </div>
+      <div> 기분 나쁜 일기 개수 : {badCount} </div>
+      <div> 기분 좋은 일기 비율 : {goodRatio} </div>
       <DiaryList onRemove={onRemove} diaryList={data} onEdit={onEdit} />  { /* diaryItem에서 ondelete를 호출 할 수 있어야하므로, 그의 부모인 diaryList에 전달*/}
     </div>
   );
