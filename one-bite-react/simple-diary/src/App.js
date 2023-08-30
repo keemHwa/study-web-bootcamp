c9f50fc24051a5b6074b7935df3f7acd437ef211
keemHwa <keem2@naver.com>
2023-08-30 20:18:21 +0900

    :memo: UseReducer - 복잡한 상태관리 로직을 컴포넌트에서 분리

--- one-bite-react/simple-diary/src/App.js
+++ one-bite-react/simple-diary/src/App.js
@@ -1,4 +1,4 @@
-import { useMemo, useState,useRef, useEffect, useCallback } from 'react';
+import { useMemo, useState,useRef, useEffect, useCallback, useReducer } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -6,9 +6,37 @@ import DiaryList from './DiaryList';
 // import Lifecycle from './Lifecycle';
 // import LifecycleUnmount from './LifecycleUnmount';
 
+
+const reducer = (state, action) => {
+  switch (action.type) { // return 된 값으로 상태가 변한다.
+    case 'INIT': {
+      return action.data //이 값으로 새로운 state(상태)가 된다.
+    }
+    case 'CREATE': {
+      const create_date = new Date().getTime();
+      const newItem = { ...action.data, create_date: create_date };
+      return [newItem, ...state]; // 기존 데이터(=state);
+    }
+    case 'REMOVE': {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    case 'EDIT': {
+      return state.map((it) => it.id === action.targetId ?
+        { ...it, content: action.newContent } : it);
+    }
+    default:
+    return state;
+  }
+}
+
+
 function App() {
 
-  const [data, setData] = useState([]); // 일기배열 
+  // const [data, setData] = useState([]); // 일기배열 
+  
+  const [data, dispatch] = useReducer(reducer, []);// App 컴포넌트에서 상태변화로직(onCreate..같은)을 분리 할 수 있는 useReducer hook
+    // dispatch(상태변화함수)는 함수형 업데이트 상관없이 호출하면 현재의 state를 reucer 함수가 참조하여 자동으로 하기에, usecallback과 사용시 dependency array를 걱정하지 않아도 된다. 
+
   const dataId = useRef(0); //useRef의 일반적인 변수 대신 사용 (어떤 dom도 선택 X, 0을가르킴 )
   
   const getDate = async () => {
@@ -25,8 +53,8 @@ function App() {
         id: dataId.current++
       }
     })
-
-    setData(initData);
+    dispatch({ type: 'INIT', data: initData });
+    // setData(initData);
   }
 
   useEffect(() => {
@@ -36,33 +64,43 @@ function App() {
  
   // 리액트는 데이터가 단방향으로 흐르기때문에, data를 변경할 수 있는 setData를 밑으로 보내주어 data를 변경 할 수 있게 해야한다.
   const onCreate = useCallback(
-    (author, content, emotion) => { // useCallback을 쓰는 이유 : data 길이 상관없이 컴포넌트가 mount되는 시점에 한번만 생성해서, 이후 일기 삭제나 기타 동작 시 재생성 되지 않도록 하기위해  
-                                      // => 이후 data추가시 기존에 데이터는 사라지고 새로 추가한 데이터만 들어오는 이슈 발생 !
-                                          // 왜냐하면 생성 당시 data는 빈 배열(현재 data값을 가져오지 못함) 이기에 이후 onCreate시 빈배열에 data가 추가된다.
-                                          // 이걸 고치려면 dependency array에 data를 넣고 data가 변경 될 때 재생성하게끔 해야하는 딜레마에 빠진다.
-                                          // 함수형 업데이트(상태변화 함수(set)에 함수를 전달) 를 사용하면 된다. ex) setData((data)=>...)
-                                          // 이렇게 함으로써 한번만 생성하고, 추가 시 최신의 데이터를 가져와서 추가 할 수있다.  -> 연관없는 action을 줄이는..!
-    console.log("onCreate ! ! ");
-    const create_date = new Date().getTime();
-    const newItem = { // 단축 속성명 : 키와 값의 이름이 같을 경우 사용할 수있다.
-      author, // author: author 
-      content,
-      emotion,
-      create_date,
-      id: dataId.current
-    }
+    (author, content, emotion) => {
+      /* useCallback을 쓰는 이유
+        - data 길이 상관없이 컴포넌트가 mount되는 시점에 한번만 생성해서, 이후 일기 삭제나 기타 동작 시 재생성 되지 않도록 하기위해
+      => 이후 data추가시 기존에 데이터는 사라지고 새로 추가한 데이터만 들어오는 이슈 발생 !
+        왜냐하면 생성 당시 data는 빈 배열(현재 data값을 가져오지 못함) 이기에 이후 onCreate시 빈배열에 data가 추가된다.
+        이걸 고치려면 dependency array에 data를 넣고 data가 변경 될 때 재생성하게끔 해야하는 딜레마에 빠진다.
+        함수형 업데이트(상태변화 함수(set)에 함수를 전달) 를 사용하면 된다. ex) setData((data) => [newItem, ...data])
+      이렇게 함으로써 한번만 생성하고, 추가 시 최신의 데이터를 가져와서 추가 할 수있다.  -> 연관없는 action을 줄이는..!
+      */
+
+      console.log("test");
+      dispatch({
+        type: 'CREATE',
+        data: { author, content, emotion, id: dataId.current }
+      });
+    // const create_date = new Date().getTime();
+    // const newItem = { // 단축 속성명 : 키와 값의 이름이 같을 경우 사용할 수있다.
+    //   author, // author(키): author (인자)
+    //   content,
+    //   emotion,
+    //   create_date,
+    //   id: dataId.current
+    // }
     dataId.current += 1;
     //setData([newItem, ...data]); // 새 데이터 +  기존 데이터 전개
-      setData((data) => [newItem, ...data]); // 최신의 state 인자로 가져오게된다. 
+    //setData((data) => [newItem, ...data]); // 최신의 state 인자로 가져오게된다. 
   },[]); // dependency가 없으므로 최초 한번만 생성
 
   const onRemove = useCallback((targetId) => { // onRemove는 한번만 생성되고 -> 재생성되면서 useEffect을 타는일은 없을 것 
-    //console.log(`전달 ${targetId}`); // 해당 id 를 가진 요소를 제외한 새로운 배열을 반환 -> data 상태가 변했기 때문에 dataList가 다시 렌더 
-    setData(data => data.filter((it) => it.id !== targetId)); // 최신의 state
+    //console.log(`전달 ${targetId}`); // 해당 id 를 가진 요소를 제외한 새로운 배열을 반환 -> data 상태가 변했기 때문에 dataList가 다시 렌더
+    //setData(data => data.filter((it) => it.id !== targetId)); // 최신의 state
+    dispatch({ type: 'REMOVE', targetId });
   }, []);
   
   const onEdit = useCallback((targetId, newContent) => {
-    setData(data=>data.map((it) => it.id === targetId ? { ...it, content: newContent } : it));
+    //setData(data=>data.map((it) => it.id === targetId ? { ...it, content: newContent } : it));
+    dispatch({ type: 'EDIT', targetId, newContent });
   }, []);
 
   // 감정 점수 비율
