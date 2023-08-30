c42994206e767f3ef97b1b1cae183a8e7bab766f
keemHwa <keem2@naver.com>
2023-08-31 02:54:23 +0900

    :memo: 상태관리, stateContext, dispatchContext 셋팅

--- one-bite-react/emotion-diary/src/App.js
+++ one-bite-react/emotion-diary/src/App.js
@@ -1,6 +1,8 @@
+import React, { useReducer, useRef } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home'; 
 import New from './pages/New'; 
@@ -12,30 +14,87 @@ import Diary from './pages/Diary';
 import MyButton from './components/MyButton';
 import MyHeader from './components/Myheader';
 
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case 'INIT':
+      return action.data;
+    case 'CREATE': {
+      newState = [action.data, ...state];
+      break; // 없으면 다음 case문을 실행한다. 
+    }
+    case 'REMOVE': {
+      newState = state.filer((it) => it.id !== action.targetId);
+      break;
+    }
+    case 'EDIT': {
+      newState = state.map((it) => it.id === action.data.id ? { ...action.data } : it);
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+}
+
+export const DiaryStateContext = React.createContext(); // context API
+export const DiaryDispatchContext = React.createContext(); // context API
 
 function App() {
+
+  const [data, dispatch] = useReducer(reducer, []);
+  
+  const dataId = useRef(1);
+  
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id : dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion 
+      }
+    })
+    dataId.current++;
+  }
+  // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({
+      type: 'REMOVE',
+      targetId
+    })
+  }
+  // EDIT 
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: 'EDIT',
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    })
+  }
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader
-          headerText={'APP'}
-          leftChild={<MyButton text={'왼쪽버튼'} onClick={() => alert('오모나')} />}
-          rightChild={<MyButton text={'오른쪽버튼'} onClick={()=>alert('오모나 오')}/>}
-        />
-        <h2>App.js</h2>{/* 어떤 컴포넌트에서든 다 보여야한다면 Routes밖에 뺀다. */}
-        <MyButton text={'버튼'} onClick={() => alert('test')} type={'positive'} />
-        <MyButton text={'버튼'} onClick={() => alert('test')} type={'negative'} />
-        <MyButton text={'버튼'} onClick={() => alert('test')} type={'dsfssfsfsfsdfds'} />
-        {/* <RouteTest/> */}
-        <Routes> {/* Routes 안에 있는 컴포넌트만 경로 따라간다. */}
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/new' element={<New/>}></Route>
-          <Route path='/edit' element={<Edit/>}></Route>
-          <Route path='/diary/:id' element={<Diary/>}></Route>
-          {/* 컴포넌트와 실제 경로 매핑 */}
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{ onCreate, onEdit, onRemove }}>
+        <BrowserRouter>
+        <div className="App">
+          {/* <RouteTest/> */}
+          <Routes> {/* Routes 안에 있는 컴포넌트만 경로 따라간다. */}
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/new' element={<New/>}></Route>
+            <Route path='/edit' element={<Edit/>}></Route>
+            <Route path='/diary/:id' element={<Diary/>}></Route>
+            {/* 컴포넌트와 실제 경로 매핑 */}
+          </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>      
   );
 }
 
