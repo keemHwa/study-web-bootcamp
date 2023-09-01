4befc882363a334b0885f13cffdc9beec35f68cc
keemHwa <keem2@naver.com>
2023-09-01 16:21:13 +0900

    :memo: 스택 - 주석 추가

--- dsa-illustrated/Stack/Stack.mjs
+++ dsa-illustrated/Stack/Stack.mjs
@@ -6,12 +6,12 @@ class Stack{
     }
 
     push(data){
-        this.list.insertAt(0, data);
+        this.list.insertAt(0, data); // 무조건 첫번째 index에 삽입 
     }
 
     pop(){
         try{
-            return this.list.deleteAt(0);
+            return this.list.deleteAt(0); // 무조건 첫번째 index 삭제
         } catch(e){
             return null;
         }
