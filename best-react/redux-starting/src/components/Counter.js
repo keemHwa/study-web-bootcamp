a4c976976a4b09545edf9912f7502c8a66b2a0c2
keemHwa <keem2@naver.com>
2023-09-09 22:17:42 +0900

    :memo: 리덕스 툴킷(reduxToolkit)  사용하기

--- best-react/redux-starting/src/components/Counter.js
+++ best-react/redux-starting/src/components/Counter.js
@@ -1,28 +1,43 @@
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux"; // useStore도 있지만 selector가 더 유용
+
+import { counterActions } from "../store/index";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch(); // action
+
   const counter = useSelector((state) => state.counter); // useSelector를 사용함으로써 리덕스 저장소에 자동 구독 설정한다.
   // 해당 리덕스 상태를 받는다.
   // 또한 함수를 통해 상태에서 일부분만 추출 가능
   // 구독 === useSelector로 선택한 상태는 리덕스 저장소에 데이터가 변경 될 때마다 자동으로 업데이트 되며 컴포넌트도 재실행되고 받아온 최신데이터를 기반으로 렌더링을 진행한다.
   const show = useSelector((state) => state.showCounter);
+  // useSelector 함수의 동작은 Redux 스토어의 상태 구조와 일치하는 대로 동작
+
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    //dispatch({ type: "increment" });
+    dispatch(counterActions.increment()); // redux-tookit > 액션 객체 생성
   };
+
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    // dispatch({ type: "increase", amount: 5 });
     // 페이로드(action 객체에 추가할 수 있는 추가적인 속성, 여기선 amount)
+    dispatch(counterActions.increase(5)); // {type: SOME_UNIQUE_IDENTIFIER, payload:5} => payload 필드명을 줄 순없다.
   };
+
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    //dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
+
   const toggleCounterHandler = () => {
-    // 해당 컴포넌트에만 관련 상태는 로컬 상태로 설정(useSteate)로 하는게 맞다 .
-    // 지금은 공부용으로 . .
-    dispatch({ type: "toggle" });
+    // dispatch({ type: "toggle" });
+    dispatch(counterActions.toggle());
   };
+
+  // 해당 컴포넌트에만 관련 상태는 로컬 상태로 설정(useSteate)로 하는게 맞다 .
+  // ex) const [data,setData] = useState([]); 같은
+  // 지금은 공부용으로 . .
 
   return (
     <main className={classes.counter}>
--- best-react/redux-starting/src/store/index.js
+++ best-react/redux-starting/src/store/index.js
@@ -1,7 +1,34 @@
 // redux logic
 import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState, // initialState : initialState
+  reducers: {
+    // reducer는 객체 혹은 맵
+    // 아래의 메서드는 자동으로 최근 state 값을 받는다
+    // 여기선 액션을 했느냐에 따라 메서드가 자동호출되므로 액션 마다 if를 쓸 필요가 없다.
+    // => 보일러 플레이트 코드를 줄 일 수 있다.
+    increment(state) {
+      state.counter++; // 직접 상태를 변경 할 수있게 됐다. (그렇게 보이는 것)
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      // 추가 payload 필요, action 인자 추가
+      state.counter = state.counter + action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+/* redux-toolkit 적용 전 
 const conterReducer = (state = initialState, action) => {
   switch (action.type) {
     case "increment": {
@@ -38,8 +65,17 @@ const conterReducer = (state = initialState, action) => {
     state.counter++은 기존 counter의 상태를 직접적으로 변경한 것 
     state.counter+1은 현재값을 가져와 1을 더한 단순 계산된값을 반환한 것 
    */
-};
+//};
 
-const store = createStore(conterReducer); // 어떤 리듀서가 저장소를 변경하는지 인자로 전달 하여 저장소 생성
+// const store = createStore(conterReducer); // 어떤 리듀서가 저장소를 변경하는지 인자로 전달 하여 저장소 생성
+// const store = createStore(createSlice.reducer); // slice에 설정한 리듀서에 접근 가능
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+// slice는 여러개 일 수 있다. (데이터 마다 묶다보면 .. ) 하지만 reducer는 하나여야한다.
+//  -> configureStore를 이용해  여러개 slice.reducer를 하나로 합친다..
+// reducer: { counter: counterSlice.reducer.
+//             toggle: testSlice.reducer } => 이럴경우 useSelector 시 state.counter.counter / state.toggle.shoCounter로 접근해야함 ,
+export const counterActions = counterSlice.actions; // 액션 객체 생성  type 프로퍼티도 이미 가지고 있다. (리듀서와 이름이 같으면 됨)
 
 export default store;
