9d042989748d71d5bf7aafe798dae6b71e85cf2f
keemHwa <keem2@naver.com>
2023-09-12 12:46:23 +0900

    :memo: 리덕스 - Thunk로 비동기 코드 작성

--- best-react/redux-async/src/App.js
+++ best-react/redux-async/src/App.js
@@ -1,7 +1,8 @@
 import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { uiActions } from "./store/ui-slice";
+// import { uiActions } from "./store/ui-slice";
+import { sendCartData } from "./store/cart-slice";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -16,53 +17,62 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
-  console.log(notification);
-
+  // 논리를 action creator function 에 옮긴 redux 비동기 코드
+  // 이쪽이 컴포넌트가 lean(컴포넌트가 최소한의 로직과 상태만을 포함하고, 가능한 간결하며 목적에 집중한 상태)라고 할 수 있다.
   useEffect(() => {
-    const sendCartData = async () => {
-      // 여기서는 sendCartData 만들기만하고 호출 X
-      dispatch(
-        uiActions.showNotification({
-          status: "pending", // 보류
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://react-redux-19987-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Error! ");
-      }
-      dispatch(
-        uiActions.showNotification({
-          status: "success", // 보류
-          title: "success! ...",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return; // 처음 sendCartData를 하지 않기위해서  + notification을 보지 않기 위해서?
     }
-
-    sendCartData().catch((err) => {
-      // 여기서 sendCartData 호출
-      dispatch(
-        uiActions.showNotification({
-          status: "error", // 보류
-          title: "Error! ...",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
+
+  // 컴포넌트에 직접 해당 논리를 부여한  redux 비동기 코드
+  // useEffect(() => {
+  //   const sendCartData = async () => {
+  //     // 여기서는 sendCartData 만들기만하고 호출 X
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: "pending", // 보류
+  //         title: "Sending...",
+  //         message: "Sending cart data!",
+  //       })
+  //     );
+  //     const response = await fetch(
+  //       "https://react-redux-19987-default-rtdb.firebaseio.com/cart.json",
+  //       {
+  //         method: "PUT",
+  //         body: JSON.stringify(cart),
+  //       }
+  //     );
+  //     if (!response.ok) {
+  //       throw new Error("Error! ");
+  //     }
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: "success", // 보류
+  //         title: "success! ...",
+  //         message: "Sent cart data successfully!",
+  //       })
+  //     );
+  //   };
+
+  //   if (isInitial) {
+  //     isInitial = false;
+  //     return; // 처음 sendCartData를 하지 않기위해서  + notification을 보지 않기 위해서?
+  //   }
+
+  //   sendCartData().catch((err) => {
+  //     // 여기서 sendCartData 호출
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: "error", // 보류
+  //         title: "Error! ...",
+  //         message: "Sending cart data failed!",
+  //       })
+  //     );
+  //   });
+  // }, [cart, dispatch]);
 
   return (
     <Fragment>
--- best-react/redux-async/src/store/cart-slice.js
+++ best-react/redux-async/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -26,6 +27,7 @@ const cartSlice = createSlice({
       }
     },
     removeItemFromCart(state, action) {
+      // 이런 reducer들을 redux toolkit 이 자동으로 생성한 action creator 생성자를 받는다.
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
@@ -37,6 +39,58 @@ const cartSlice = createSlice({
     },
   },
 });
+
+// custom action creator
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    // action obeject를 반환하지 않고, action object를 반환하는 다른 함수(여기서는 비동기함수)를 반환한다.
+    /** Redux Thunk 란..  */
+    //  Redux 애플리케이션에서 비동기 작업을 처리하고 액션을 디스패치할 수 있도록 도와주는 도구입니다.
+    // Redux Thunk를 사용하면 Redux 스토어의 액션 생성자 함수가 반환하는 것이 일반적인 액션 객체뿐만 아니라 함수도 될 수 있습니다.
+    // 이 함수는 dispatch와 getState 매개변수를 인자로 받아서 비동기 작업을 수행하고 필요한 액션을 디스패치할 수 있게 해줍니다.
+    // thuck 함수를 호출 할 때 dispatch 함수는 Redux Thunk에서 자동으로 제공됩니다.
+    dispatch(
+      uiActions.showNotification({
+        status: "pending", // 보류
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-redux-19987-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Error! ");
+      }
+    };
+
+    try {
+      sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success", // 보류
+          title: "success! ...",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (err) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error", // 보류
+          title: "Error! ...",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
